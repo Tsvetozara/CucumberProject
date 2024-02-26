30650d7207a5d62ce7c635a0aca4cf9d7cc26bce
Tsvetozara <tsvetozara.beneva@gmail.com>
2024-02-26 15:02:41 +0200

    Added Cucumber HTML Report

--- src/test/java/tests/Runner.java
+++ src/test/java/tests/Runner.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/java/tests/features")
+        features = "src/test/java/tests/features",
+        plugin = {"pretty","html:target/CucumberHTMLReport.html"})
 public class Runner {
 }
